174de50bf576fe8c14545798129af61521640b05
npuchko <npuchko@adobe.com>
2022-08-16 14:12:08 -0500

    Updated some checks for Advanced Reporting

--- magento_support_checker.php
+++ magento_support_checker.php
@@ -11,6 +11,7 @@ namespace {
     $checksList = [
         'advanced_reporting' => [
             \MagentoSupport\SupportChecker\EnabledChecker::class,
+            \MagentoSupport\SupportChecker\FailedSubscriptionChecker::class,
             \MagentoSupport\SupportChecker\IsMultipleCurrency::class,
             \MagentoSupport\SupportChecker\CronDbCheck::class,
             \MagentoSupport\SupportChecker\TokenChecker::class,
@@ -68,6 +69,9 @@ namespace MagentoSupport\SupportChecker {
 
     use Magento\Analytics\Model\Config\Backend\CollectionTime;
     use Magento\Analytics\Model\ReportUrlProvider;
+    use Magento\Analytics\Model\SubscriptionStatusProvider;
+    use Magento\Framework\App\Config\ScopeConfigInterface;
+    use Magento\Store\Model\Store;
     use Symfony\Component\Console\Input\InputInterface;
     use Symfony\Component\Console\Output\OutputInterface;
     use Magento\Framework\App\ResourceConnection;
@@ -122,6 +126,20 @@ namespace MagentoSupport\SupportChecker {
 
     class EnabledChecker extends AbstractDbChecker
     {
+        /**
+         * @var \Magento\Analytics\Model\SubscriptionStatusProvider
+         */
+        private $subscriptionStatusProvider;
+
+        public function __construct(
+            ResourceConnection $resource,
+            \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig,
+            \Magento\Analytics\Model\SubscriptionStatusProvider $subscriptionStatusProvider
+        ) {
+            parent::__construct($resource, $scopeConfig);
+            $this->subscriptionStatusProvider = $subscriptionStatusProvider;
+        }
+
         public function getName()
         {
             return 'Is Enabled';
@@ -129,6 +147,16 @@ namespace MagentoSupport\SupportChecker {
 
         public function execute(InputInterface $input, OutputInterface $output)
         {
+            $subscriptionStatus = $this->subscriptionStatusProvider->getStatus();
+            $output->writeln('');
+            $output->writeln('Subscription status: ' . $subscriptionStatus);
+            if (SubscriptionStatusProvider::PENDING === $subscriptionStatus) {
+                $output->writeln('<error>Check for cron job analytics_subscribe</error>');
+            }
+
+            if (SubscriptionStatusProvider::FAILED === $subscriptionStatus) {
+                $output->writeln('<error>Check for cron job analytics_subscribe in the past</error>');
+            }
             $isError = false;
             $dbRows = $this->selectFromCoreConfig(
                 ['scope', 'scope_id', 'value'],
@@ -154,6 +182,40 @@ namespace MagentoSupport\SupportChecker {
             if (!$isModuleEnabledByConfig) {
                 $output->writeln("<error>Module disabled in app/etc/config.php or app/etc/env.php</error>");
                 return false;
+            }
+
+            return true;
+        }
+    }
+
+    class FailedSubscriptionChecker extends AbstractDbChecker {
+
+        private SubscriptionStatusProvider $subscriptionStatusProvider;
+
+        public function __construct(
+            ResourceConnection $resource,
+            \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig,
+            \Magento\Analytics\Model\SubscriptionStatusProvider $subscriptionStatusProvider
+        ) {
+            parent::__construct($resource, $scopeConfig);
+            $this->subscriptionStatusProvider = $subscriptionStatusProvider;
+        }
+
+        public function getName()
+        {
+            return 'Failed Subscription Checker';
+        }
+
+        public function execute(InputInterface $input, OutputInterface $output)
+        {
+            $status = $this->subscriptionStatusProvider->getStatus();
+
+            if (
+                $status === \Magento\Analytics\Model\SubscriptionStatusProvider::FAILED
+                || $status === \Magento\Analytics\Model\SubscriptionStatusProvider::PENDING
+            ) {
+                $url = $this->scopeConfig->getValue(Store::XML_PATH_SECURE_BASE_URL);
+                $output->writeln('URL for subscription: ' . $url);
             }
 
             return true;
